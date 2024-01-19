fa428fb3c29fd2f1c9dbd5aa1e0198ffc89d37f7
Michael Denness <thisisturboproductions@gmail.com>
2024-01-19 11:35:26 -0500

    Followed Travis' Example

---
+++ script.js
@@ -0,0 +1,16 @@
+// Creates a string with the intro phrase:
+const introMessage = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
+console.log(introMessage)
+
+// Creates 3 variables: First must equal 10, second 40, and third 39:
+const num1 = 2 * 5
+const num2 = 8 + (8 * 4)
+const num3 = 289 % 50
+
+console.log('Number 1:', num1)
+console.log('Number 2:', num2)
+console.log('Number 3:', num3)
+
+// Creates a dialogue box with the values:
+alert(introMessage + '\n' + num1 + ' - ' + num2 + ' - ' + num3)
